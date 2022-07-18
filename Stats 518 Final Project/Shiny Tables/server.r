ed83eb4744ef96796c504b6d38a7c7d28ffbb00b
Brooke <99227900+BrookemWalters@users.noreply.github.com>
2022-07-17 20:55:53 -0400

    Update server.r

--- Stats 518 Final Project/Shiny Tables/server.r
+++ Stats 518 Final Project/Shiny Tables/server.r
@@ -1,1 +1,30 @@
+library("DT")
+library("tidyverse")
+
+
+
+
+sum_table_cc <- read_csv("~/STA 518/Stat-518-Portfolio/Final Project/Mi_Covid/data/covid_census_summary_stats.csv")
+entire_table_cc <- read_csv("~/STA 518/Stat-518-Portfolio/Final Project/Mi_Covid/data/Covid_Census_Table.csv")
+
+
+function(input, output, session){
+  
+  
+  
+  output$sum_table_cc<- renderDT({
+    sum_table_cc %>% select(-1)})
+
+    output$entire_table_cc<- renderDT({
+      entire_table_cc %>% select(-1) })
+    
+    
+    output$starwars<- renderDT({
+      starwars})
+    
+  }
+
+
+
+
 
